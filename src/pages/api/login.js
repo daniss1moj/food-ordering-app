a024f90ae035dfda9fd8fe92190c9bcb73e22d88
Danyil <onishchenkodanyil11v@gmail.com>
2023-02-07 20:49:23 +0200

    fix

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -3,7 +3,10 @@ import cookie from 'cookie';
 const handler = (req, res) => {
 	if (req.method === 'POST') {
 		const { username, password } = req.body;
-		if (username === process.env.ADMIN_USERNAME && password === process.env.ADMIN_PASSWORD) {
+		if (
+			username === process.env.NEXT_PUBLIC_ADMIN_USERNAME &&
+			password === process.env.NEXT_PUBLIC_ADMIN_PASSWORD
+		) {
 			res.setHeader(
 				'Set-Cookie',
 				cookie.serialize('token', process.env.TOKEN, {
